72428ded60edfc33768bb9ba9b2a0ff588fd3c1f
Arko Chakravarty <57612965+Arko2019@users.noreply.github.com>
2025-03-05 00:07:33 +0530

    Update Java8AllInOne.java

--- InterviewQuestion/src/com/java/java8feature/Java8AllInOne.java
+++ InterviewQuestion/src/com/java/java8feature/Java8AllInOne.java
@@ -1,32 +1,44 @@
-package com.java.java8feature;
+package interview;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Java8AllInOne {
 
+//		Counting frequency of each character
+//		Return 2nd largest number
+//		Make the 2nd character of every word upperCase
+//		Square each number in the list and collect the result in a new list
+//		group words by their lengths and collect the result into a Map.
+//		find sum of all nos in a list
+
 	public static void main(String[] args) {
 		List<Employee> list = new ArrayList<>();
 
-		list.add(new Employee(1, "AAAA", 35, "Marketing", 2000, Arrays.asList(1, 2, 3, 4, 5)));
+		list.add(new Employee(1, "AAAA", 35, "Marketing", 2000,
+				Arrays.asList(11111111, 2222222, 3333333, 444444, 555555)));
 		list.add(new Employee(6, "BBBB", 36, "Sales", 4000, Arrays.asList(7, 8, 9)));
 		list.add(new Employee(3, "CCCC", 37, "HR", 5000, Arrays.asList(11, 21, 31, 41, 51)));
 		list.add(new Employee(2, "DDDD", 38, "Development", 6000, Arrays.asList(1, 2, 3, 4, 5)));
 		list.add(new Employee(4, "EEEE", 35, "Marketing", 2000, Arrays.asList(12, 23, 43, 44, 54)));
 		list.add(new Employee(5, "FFFF", 35, "Sales", 9000, Arrays.asList(12, 23, 43, 44, 54)));
 		list.add(new Employee(7, "GGGG", 35, "Sales", 9000, Arrays.asList(12, 23, 43, 44, 54)));
+		String str = "Hello";
 
+		List<Integer> list2 = Arrays.asList(1, 2, 1, 3, 4, 5, 1, 2, 6, 9, 7, 8);
+//		int a[]={2,2,2,10,15,8,39,10,35,88,62,75};
+//		List<int[]> list2 =Arrays.asList(a);
 		// printEmployee(list);
-		// checkSalary(list);
+		// checkSalary(list); // l.salary > 4000
 		// printEmployeeName(list);// who is sales
 		// heighestSalary(list);
 		// minimumSalary(list);
@@ -44,24 +56,69 @@ public class Java8AllInOne {
 		// convertListToMapAndSortInAssOrder(list);
 		// convertListToMapAndSortInDescOrder(list);
 		// maxSalaryOfEmployeeBasedOnDepartment(list);
-		convertingPrimitiveStreamsToObjectStream(list);
+		// convertingPrimitiveStreamsToObjectStream(list);
+//				Return 2nd largest number
+		// return2ndLargestNo(list);
+		// findFrequencyOfCharacter(str);
+		// removeDublicateAndSort(list2);
+		// findDublicatesInList(list3);
+		secondHeighestNo(list2);
+	}
+	static void reversePairs() {
+		int[] arr = {10, 20, 30, 40, 50, 60, 70, 80}; 
+		int[] result= IntStream.range(0, arr.length)
+		 .map(i-> (i%2==0 && i+1 <arr.length) ? arr[i+1]:arr[i-1]).toArray();
+		System.out.println(Arrays.toString(result));
+	}
+	void reverseWordInString() {
+		String s = "Hello How Are you";
+
+		// s.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+		String[] s1 = s.split(" ");
+		List<String> list = Arrays.asList(s1);
+		List<String> words = list.stream().map(k -> new StringBuilder(k).reverse().toString()).toList();
+		String result = String.join(" ", words);
+		System.out.println(result);
 	}
 
-	
+	private static void removeDublicateAndSort(List<Integer> list2) {
+		list2.stream().distinct().sorted().toList();
+	}
+
+	private static void secondHeighestNo(List<Integer> list2) {
+		Optional<Integer> secondLargest = list2.stream().distinct().sorted(Comparator.reverseOrder()).skip(1)
+				.findFirst();
+		System.out.println(secondLargest.get());
+
+	}
+
+	private static void findFrequencyOfCharacter(String str) {
+
+		str.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+//		Map<Character, Long> frequency = str.chars().mapToObj(c -> (char) c)
+//				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//		System.out.println(frequency);
+	}
+
+	private static void return2ndLargestNo(List<Employee> list) {
+		Optional<Integer> emp = list.stream().sorted(Comparator.comparing(Employee::getSalary).reversed())
+				.map(Employee::getSalary).distinct().limit(2).skip(1).findFirst();
+
+		System.out.println(emp.get());
+	}
+
 	private static void convertingPrimitiveStreamsToObjectStream(List<Employee> list) {
 		// TODO Auto-generated method stub
-		IntStream intStream = list
-                .stream()
-                .mapToInt(Employee::getSalary);
-        System.out.println(intStream.boxed());
+		IntStream intStream = list.stream().mapToInt(Employee::getSalary);
+		System.out.println(intStream.boxed());
 	}
 
-
 	private static void maxSalaryOfEmployeeBasedOnDepartment(List<Employee> list) {
-		Map<String, Optional<Employee>> map=list.stream().collect(Collectors.groupingBy(Employee::getDepartment,
+		Map<String, Optional<Employee>> map = list.stream().collect(Collectors.groupingBy(Employee::getDepartment,
 				Collectors.reducing(BinaryOperator.maxBy(Comparator.comparing(Employee::getSalary)))));
-				System.out.println(map);
-		
+		System.out.println(map);
+
 	}
 
 	private static void convertListToMapAndSortInDescOrder(List<Employee> list) {
@@ -76,8 +133,7 @@ public class Java8AllInOne {
 
 	private static void convertListToMapAndSortInAssOrder(List<Employee> list) {
 		Map<Integer, String> map = list.stream().collect(Collectors.toMap(Employee::getId, Employee::getName));
-		Map<Integer, String> map2 = map.entrySet().stream()
-				.sorted(Map.Entry.<Integer, String>comparingByKey())
+		Map<Integer, String> map2 = map.entrySet().stream().sorted(Map.Entry.<Integer, String>comparingByKey())
 				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o, n) -> o, LinkedHashMap::new));
 		System.out.println(map2);
 	}
@@ -89,7 +145,7 @@ public class Java8AllInOne {
 	}
 
 	private static void mergeAndPrintPhoneNo(List<Employee> list) {
-		List<Integer> listPhNo = list.stream().map(k -> k.getPhNo()).flatMap(Collection::stream).toList();
+		List<Integer> listPhNo = list.stream().map(k -> k.getPhNo()).flatMap(l -> l.stream()).toList();
 		System.out.println(listPhNo);
 	}
 
@@ -109,7 +165,6 @@ public class Java8AllInOne {
 	private static void sortEmployeeBaseOnDepartmentAndSalary(List<Employee> list) {
 		list.stream().sorted(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getId))
 				.forEach(System.out::println);
-
 	}
 
 	private static void sortEmployeeBaseOnSalaryReversed(List<Employee> list) {
@@ -149,13 +204,20 @@ public class Java8AllInOne {
 	}
 
 	private static void heighestSalary(List<Employee> list) {
-		String e = list.stream().max(Comparator.comparing(Employee::getSalary)).map(Employee::getName).get();
-		System.out.println(e);
+
+		// list.stream().max(Comparator.comparing(Employee::getSalary))
+		// .map(Employee::getName).get();
+
+		Employee e = list.stream().max(Comparator.comparing(Employee::getSalary)).get();
+//			System.out.println(e);
 
 	}
 
 	private static void printEmployeeName(List<Employee> list) {
-		list.stream().filter(k -> k.getDepartment() == "Sales").map(Employee::getName).forEach(System.out::println);
+
+		list.stream().filter(k -> k.department == "Sales").map(l -> l.getName()).forEach(System.out::println);
+		// list.stream().filter(k -> k.getDepartment() ==
+		// "Sales").map(Employee::getName).forEach(System.out::println);
 
 	}
 
